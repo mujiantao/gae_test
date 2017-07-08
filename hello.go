3f3e39d789d81a6119bc744888ecacc1ca62f771
timu <timu@timudeMacBook-Pro.local>
2017-07-09 01:00:30 +0800

    add

--- hello.go
+++ hello.go
@@ -1,39 +1,24 @@
 package hello
 
 import (
-    "fmt"
-    "net/http"
-    "google.golang.org/appengine"
-    "google.golang.org/appengine/urlfetch"
-    "io/ioutil"
+	"net/http"
+	"net/http/httputil"
+	"net/url"
+
+	"appengine"
+	"appengine/urlfetch"
 )
+
 func init() {
-    http.HandleFunc("/", handler)
+	http.HandleFunc("/", serve)
 }
 
-var headers = map[string]string{
-	"User-Agent": "MQQBrowser/26 Mozilla/5.0 (Linux; U; Android 2.3.7; zh-cn; MB200 Build/GRJ22; CyanogenMod-7) AppleWebKit/533.1 (KHTML, like Gecko) Version/4.0 Mobile Safari/533.1",
-	"Referer":    "https://yun.baidu.com/share/home?uk=325913312#category/type=0",
-	"Cookie":     "",
-}
+func serve(w http.ResponseWriter, r *http.Request) {
+	c := appengine.NewContext(r)
 
-func handler(w http.ResponseWriter, r *http.Request) {
-        ctx := appengine.NewContext(r)
-        client := urlfetch.Client(ctx)
-        req, err := http.NewRequest("GET", "https://www.qzhou.com.cn/", nil)
+	url := &url.URL{Path: "http://localhost:8888"}
 
-        for k, v := range headers {
-      		req.Header.Set(k, v)
-      	}
-
-        var resp *http.Response
-      	resp, err = client.Do(req)
-        if err != nil {
-                http.Error(w, err.Error(), http.StatusInternalServerError)
-                return
-        }
-        var body []byte
-      	body, err = ioutil.ReadAll(resp.Body)
-        defer resp.Body.Close()
-        fmt.Fprintf(w, string(body))
+	proxy := httputil.NewSingleHostReverseProxy(url)
+	proxy.Transport = &urlfetch.Transport{Context: c}
+	proxy.ServeHTTP(w, r)
 }
